2d1dfaa7758aa0f0638148eccba10841ffc9425f
Brian Ogungbe <99332476+B00132889@users.noreply.github.com>
2024-04-08 21:03:41 +0100

    Added Calendar

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/customers/edit/{id}', 'App\Http\Controllers\CustomerController@edit
 Route::post('/customers/update', 'App\Http\Controllers\CustomerController@update');
 Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
 Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
+Route::get('/calendar/display', 'App\Http\Controllers\calendarController@display');
 
 
 require __DIR__.'/auth.php';
