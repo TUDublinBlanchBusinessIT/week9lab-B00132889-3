a30390b5b63824128d37751f33bef12cf765ce32
Brian Ogungbe <99332476+B00132889@users.noreply.github.com>
2024-04-08 15:16:55 +0100

    Intial Commit

---
+++ resources/views/layouts/navAuth.blade.php
@@ -0,0 +1,5 @@
+<ul class="nav navbar-nav pull-right"> 
+    <li><a href="#register">Register</a></li> 
+    <li><a href="#Login">Login</a></li> 
+    <li><a href="#Logoff">Logoff</a></li> 
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/customers/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
+Route::post('/customers/update', 'App\Http\Controllers\CustomerController@update');
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
+
+
+require __DIR__.'/auth.php';
